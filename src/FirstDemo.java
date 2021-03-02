60f2aa30e36a06fbd6ec8ea9e830b1852d86ee81
Max-maker-gif <1611760583@qq.com>
2021-03-02 13:55:37 +0800

    test

--- src/FirstDemo.java
+++ src/FirstDemo.java
@@ -2,5 +2,6 @@ public class FirstDemo {
     public static void main(String[] args) {
         System.out.println("This is a FirstDemo");
         System.out.println("okok");
+        System.out.println("111");
     }
 }
